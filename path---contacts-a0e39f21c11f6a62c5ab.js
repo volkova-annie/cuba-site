ac06a92131545495a021f05d4352d70e4e6eaa40
Alexander Kurganow <me@akurganow.ru>
2017-10-29 18:57:54 +0300

    Updates

---
+++ path---contacts-a0e39f21c11f6a62c5ab.js
@@ -0,0 +1,2 @@
+webpackJsonp([0x3516f1ec4b950200],{"./node_modules/json-loader/index.js!./.cache/json/contacts.json":function(o,n){o.exports={pathContext:{}}}});
+//# sourceMappingURL=path---contacts-a0e39f21c11f6a62c5ab.js.map
