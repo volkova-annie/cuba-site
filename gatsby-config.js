db5fdd6d9535f16fd8b4222eb10a2d0797013deb
Alexander Kurganov <me@akurganow.ru>
2017-12-25 15:42:47 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,8 @@ module.exports = {
     {
       resolve: 'gatsby-source-contentful',
       options: {
-        spaceId: 'pp17w473qhjl',
-        accessToken: 'c75731e78862f3356bf3245ecdefc5118711b4df9168b8a229d5c3ba028e66e9',
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_TOKEN,
       },
     },
   ],
